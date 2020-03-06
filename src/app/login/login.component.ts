97da5f063cbbdcaca4b80309e19862318d7d6220
Marcos Sanz González <msanz@profile.local>
2020-03-06 18:58:03 +0100

    feat: acceso por token

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,8 @@ import { NgForm } from '@angular/forms';
 import { UsuarioService } from '../services/service.index';
 import { Usuario } from '../models/usuario.model';
 
+declare const gapi: any;
+
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
@@ -14,6 +16,8 @@ export class LoginComponent implements OnInit {
     recuerdame = false;
     email: string;
 
+    auth2: any;
+
     constructor(
         public router: Router,
         public usuarioService: UsuarioService
@@ -21,10 +25,31 @@ export class LoginComponent implements OnInit {
 
     ngOnInit(): void {
         // Fijamos el mail si está recordado en el localstorage.
+        this.googleInit();
         this.email = localStorage.getItem('email') || '';
-        if ( this.email.length > 1) {
+        if (this.email.length > 1) {
             this.recuerdame = true;
         }
+    }
+
+    googleInit() {
+        gapi.load('auth2', () => {
+            this.auth2 = gapi.auth2.init({
+                client_id: '361759443978-gqg4m7uchreus416gjbiv3nmiiuceodm.apps.googleusercontent.com',
+                cookiepolicy: 'single_host_origin',
+                scope: 'profile email'
+            });
+
+            this.attachSignin(document.getElementById('btnGoogle'));
+        });
+    }
+
+    attachSignin(element) {
+        this.auth2.attachClickHandler(element, {}, (googleUser) => {
+            // const profile = googleUser.getBasicProfile();
+            const token = googleUser.getAuthResponse().id_token;
+            console.log(token);
+        });
     }
 
     ingresar(forma: NgForm) {
